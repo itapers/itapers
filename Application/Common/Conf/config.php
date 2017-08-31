25f4d1a97ea36d428fa62d8fcb13225b1125128a
itapers <15978896108@163.com>
2017-08-31 12:15:48 +0800

    修改

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -30,7 +30,7 @@ return array(
         'SHOW_PAGE_TRACE' =>false,  //
         'SHOW_ERROR_MSG' =>    false,
         'MODULE_DENY_LIST'      =>  array('Common','Runtime'),
-        'URL_MODEL'             =>  1,       // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
+        'URL_MODEL'             =>  0,       // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
         // 0 (普通模式); 1 (PATHINFO 模式); 2 (REWRITE  模式); 3 (兼容模式)  默认为PATHINFO 模式
         'URL_HTML_SUFFIX'       =>  'html',  // URL伪静态后缀设置
         'ERROR_MESSAGE'  =>    '哎呦呦，一不小心就走丢了！',
